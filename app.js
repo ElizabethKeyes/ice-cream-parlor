86a0e9a3699883f74f0f35403a610914a380cb59
Elizabeth Keyes <liz4momentum@gmail.com>
2023-03-01 15:16:18 -0700

    all items add to cart successfully. Need to add cart total and calculate

--- app.js
+++ app.js
@@ -1,38 +1,134 @@
 const iceCream = [{
   name: 'Cookie Dough',
   image: 'https://celebratingsweets.com/wp-content/uploads/2014/04/Cookie-Dough-Ice-Cream-1-5.jpg',
-  price: 1
+  price: 1,
+  quantity: 0
 }, {
   name: 'Vanilla',
   image: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/ultimate-vanilla-ice-cream-1628511695.jpg',
-  price: 1
+  price: 1,
+  quantity: 0
 }, {
   name: 'Strawberry',
   image: 'https://www.realfoodwithjessica.com/wp-content/uploads/2017/07/paleostrawberryicecream2.jpg',
-  price: 2
+  price: 2,
+  quantity: 0
 }];
 
 const toppings = [{
   name: 'Sprinkles',
   image: 'https://upload.wikimedia.org/wikipedia/commons/f/f6/Sprinkles2.jpg',
-  price: 1
+  price: 1,
+  quantity: 0
 }, {
   name: 'Chocolate Chips',
   image: 'https://www.eatthis.com/wp-content/uploads/sites/4/2020/05/chocolate-chips.jpg?quality=82&strip=1&resize=640%2C360',
-  price: 2
+  price: 2,
+  quantity: 0
 }];
 
 const containers = [{
   name: 'Waffle Cone',
   image: 'https://m.media-amazon.com/images/I/71VNjBMakfL._SL1500_.jpg',
-  price: 2
+  price: 2,
+  quantity: 0
 }, {
   name: 'Waffle Bowl',
   image: 'http://images.wbmason.com/350/L_JOY66050.jpg',
-  price: 4
+  price: 4,
+  quantity: 0
 }]
 
-function addToCart(item) {
-  console.log(item, "has been added to your cart");
 
+let cNameElem = document.getElementById("container-name")
+let cQtyElem = document.getElementById("container-quantity")
+let cUnitPriceElem = document.getElementById("container-price")
+let cTotalPriceElem = document.getElementById("container-total-price")
+
+let iNameElem = document.getElementById("ice-cream-name")
+let iQtyElem = document.getElementById("ice-cream-quantity")
+let iUnitPriceElem = document.getElementById("ice-cream-price")
+let iTotalPriceElem = document.getElementById("ice-cream-total-price")
+
+let tNameElem = document.getElementById("topping-name")
+let tQtyElem = document.getElementById("topping-quantity")
+let tUnitPriceElem = document.getElementById("topping-price")
+let tTotalPriceElem = document.getElementById("topping-total-price")
+
+let cartTotal = 0
+
+
+function drawCart() {
+  cNameElem.innerHTML = ''
+  cQtyElem.innerHTML = ''
+  cUnitPriceElem.innerHTML = ''
+  cTotalPriceElem.innerHTML = ''
+  iNameElem.innerHTML = ''
+  iQtyElem.innerHTML = ''
+  iUnitPriceElem.innerHTML = ''
+  iTotalPriceElem.innerHTML = ''
+  tNameElem.innerHTML = ''
+  tQtyElem.innerHTML = ''
+  tUnitPriceElem.innerHTML = ''
+  tTotalPriceElem.innerHTML = ''
+  let cTotal = 0
+  let iCTotal = 0
+  let tTotal = 0
+
+  containers.forEach(c => {
+    if (c.quantity) {
+      cTotal = (c.price * c.quantity)
+      cNameElem.innerHTML += `<p>${c.name}</p>`
+      cQtyElem.innerHTML += `<p>${c.quantity}</p>`
+      cUnitPriceElem.innerHTML += `<p>$${c.price}</p>`
+      cTotalPriceElem.innerHTML += `<p> $${(c.price * c.quantity).toFixed(2)}</p>`
+    }
+  })
+
+  iceCream.forEach(i => {
+    if (i.quantity) {
+      iCTotal += (i.price * i.quantity)
+      iNameElem.innerHTML += `<p>${i.name}</p>`
+      iQtyElem.innerHTML += `<p>$${i.quantity}</p>`
+      iUnitPriceElem.innerHTML += `<p>$${i.price}</p>`
+      iTotalPriceElem.innerHTML += `<p> $${(i.price * i.quantity).toFixed(2)}</p>`
+    }
+  })
+  toppings.forEach(t => {
+    if (t.quantity) {
+      tTotal += (t.price * t.quantity)
+      tNameElem.innerHTML += `<p>${t.name}</p>`
+      tQtyElem.innerHTML += `<p>${t.quantity}</p>`
+      tUnitPriceElem.innerHTML += `<p>$${t.price}</p>`
+      tTotalPriceElem.innerHTML += `<p> $${(t.price * t.quantity).toFixed(2)}</p>`
+    }
+  })
+
+}
+
+function addContainer(name) {
+  console.log(name, "has been added to your cart");
+  let chosenItem = containers.find(c => c.name === name)
+  chosenItem.quantity++
+  console.log(chosenItem);
+
+  drawCart()
+}
+
+function addIceCream(name) {
+  console.log(name, "has been added to your cart");
+  let chosenItem = iceCream.find(i => i.name === name)
+  console.log(chosenItem)
+  chosenItem.quantity++
+
+  drawCart()
+}
+
+function addTopping(name) {
+  console.log(name, "has been added to your cart");
+  let chosenItem = toppings.find(t => t.name === name)
+  chosenItem.quantity++
+  console.log(chosenItem);
+
+  drawCart()
 }
